7323a9cf27692b1a76d0df6a2cbebf13525f32ab
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-08 01:01:19 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,18 +1,22 @@
-def hello_t(array)
-i = 0 
+# def hello_t(array)
+# i = 0 
 
-while i < array.count do 
-  yield array[i] 
-  i = i + 1
+# while i < array.count do 
+#   yield array[i] 
+#   i = i + 1
   
-end   
-end
+# end   
+# end
 
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end 
-end 
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end 
+# end 
+
+def hello_t(array)
+  
+  
